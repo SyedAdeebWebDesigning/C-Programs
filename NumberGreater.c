2c6e709a587558e849217ce7951522c949de6bcc
Syed Adeeb <adeeb@Syeds-MacBook-Pro.local>
2023-10-06 15:13:17 +0530

    modified:   NumberGreater.c

--- NumberGreater.c
+++ NumberGreater.c
@@ -2,19 +2,25 @@
 
 int main()
 {
-    int num1, num2;
+    int num1, num2, num3;
     printf("Enter the number 1: ");
     scanf("%d", &num1);
     printf("Enter the number 2: ");
     scanf("%d", &num2);
+    printf("Enter the number 3: ");
+    scanf("%d", &num3);
 
-    if (num1 > num2)
+    if (num1 > num2 && num1 > num3)
     {
-        printf("%d is greater than %d\n", num1, num2);
+        printf("Num1: %d is greater\n", num1);
     }
-    else
+    else if (num2 > num3 && num2 > num1)
     {
-        printf("%d is greater than %d\n", num2, num1);
+        printf("Num2: %d is greater\n", num2);
+    }
+    else if (num3 > num1 && num3 > num2)
+    {
+        printf("Num3: %d is greater\n", num3);
     }
     return 0;
 }
